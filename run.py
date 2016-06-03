7b8f5f000d9f0325cc2f0491fc5ae580bd52a156
Vivek Nair <vivekaxl@gmail.com>
2016-06-03 16:54:07 -0400

    started normalization

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
 from src.data_splitting import split_data, data_cleaning, vectorizing
+import pandas as pd
+
+def cleaning():
+    filename = data_cleaning("bank.csv")
+    vectorizing(filename)
+
+def normalization(filename):
+    foldername = "./norm_data/"
+    df = pd.read_csv(filename)
+    columns = df.columns
+    independent = columns[:-1]
+    dependent = columns[-1]
+    independent_df = df[independent]
+    df_norm = (independent_df - independent_df.mean()) / (independent_df.max() - independent_df.min())
+    import pdb
+    pdb.set_trace()
+
 
 if __name__ == "__main__":
-    # split_data("adult.csv")
-    filename = data_cleaning("bankecho ")
-    vectorizing(filename)
+
