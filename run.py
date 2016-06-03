8ddf1a6fa0b78e47457f9c8f413d47cab9e68e6a
Vivek Nair <vivekaxl@gmail.com>
2016-06-03 17:08:21 -0400

    added normalized datasets

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from src.data_splitting import split_data, data_cleaning, vectorizing
 import pandas as pd
+from os import listdir
 
 def cleaning():
     filename = data_cleaning("bank.csv")
@@ -13,9 +14,16 @@ def normalization(filename):
     dependent = columns[-1]
     independent_df = df[independent]
     df_norm = (independent_df - independent_df.mean()) / (independent_df.max() - independent_df.min())
-    import pdb
-    pdb.set_trace()
+    df_norm['class'] = df[dependent]
+    df_norm.to_csv(foldername + filename.split("/")[-1], index=False)
 
+
+def _normalization():
+    foldername = "./Data/"
+    files = [foldername + f for f in listdir(foldername)]
+    files.remove('./Data/.DS_Store')
+    files.remove('./Data/__init__.py')
+    for file in files:
+        normalization(file)
 
 if __name__ == "__main__":
-
